2dc95114680f6b8fc42c8bee03efdf89e7d991a1
Rafaqat Ali <83506643+mianrafaqat@users.noreply.github.com>
2023-06-01 14:47:30 +0500

    reasons section

---
+++ src/components/Reasons/Reasons.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "./Reasons.css";
+import image1 from "../../assets/image1.png";
+import image2 from "../../assets/image2.png";
+import image3 from "../../assets/image3.png";
+import image4 from "../../assets/image4.png";
+import nb from "../../assets/nb.png";
+import adidas from "../../assets/adidas.png";
+import nike from "../../assets/nike.png";
+import tick from "../../assets/tick.png";
+
+const Reasons = () => {
+  return (
+    <div className="Reasons" id="reasons">
+      <div className="left-r">
+        <img src={image1} alt="rImg" />
+        <img src={image2} alt="rImg" />
+        <img src={image3} alt="rImg" />
+        <img src={image4} alt="rImg" />
+      </div>
+      <div className="right-r">
+        <span>Some Reasons</span>
+        <div>
+          <span className="stroke-text">why</span>
+          <span> choose us</span>
+        </div>
+
+        <div className="details-r">
+          <div>
+            <img src={tick} alt="brand" />
+            <span>OVER 140+ EXPERT COACHS</span>
+          </div>
+          <div>
+            <img src={tick} alt="brand" />
+            <span>TRAIN SMARTER AND FASTER THAN BEFORE</span>
+          </div>
+          <div>
+            <img src={tick} alt="brand" />
+            <span>1 FREE PROGRAM FOR NEW MEMBER</span>
+          </div>
+          <div>
+            <img src={tick} alt="brand" />
+            <span>RELIABLE PARTNERS</span>
+          </div>
+        </div>
+
+        <span style={{ color: "var(--gray)", fontWeight: "normal" }}>
+          OUR PARTNERS
+        </span>
+        <div className="partners">
+          <img src={nb} alt="partners" />
+          <img src={adidas} alt="partners" />
+          <img src={nike} alt="partners" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Reasons;
