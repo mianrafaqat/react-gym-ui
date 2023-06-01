1c2cdd597c0ad3079b020e423f700972de15ceff
Rafaqat Ali <83506643+mianrafaqat@users.noreply.github.com>
2023-06-01 12:01:28 +0500

    hero section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import logo from "../../assets/logo.png";
 const Header = () => {
   return (
     <div className="header">
-      <img src={logo} className="logo" />
+      <img src={logo} className="logo" alt="logo" />
 
       <ul className="header-menu">
         <li>Home</li>
--- src/components/Heo/Hero.jsx
+++ src/components/Heo/Hero.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Hero.css";
 import Header from "../Header/Header";
+import hero_img from "../../assets/hero_image.png";
+import hero_img_back from "../../assets/hero_image_back.png";
+import Heart from "../../assets/heart.png";
+import Calories from "../../assets/calories.png";
 
 const Hero = () => {
   return (
@@ -28,9 +32,52 @@ const Hero = () => {
             </span>
           </div>
         </div>
+
+        <div className="figures">
+          <div>
+            <span>+140</span>
+            <span>expert coaches</span>
+          </div>
+          <div>
+            <span>+978</span>
+            <span>members joined</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span>fitness programs</span>
+          </div>
+        </div>
+
+        <div className="hero-buttons">
+          <button className="btn">Get Started</button>
+          <button className="btn">Learn More</button>
+        </div>
       </div>
 
-      <div className="right-h">Right Side</div>
+      <div className="right-h">
+        <button className="btn">Join Now</button>
+
+        <div className="heart-rate">
+          <img src={Heart} alt="heart" />
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
+        </div>
+
+        <img src={hero_img} alt="heroImg" className="hero-image" />
+        <img
+          src={hero_img_back}
+          alt="heroImgBack"
+          className="hero-image-back"
+        />
+
+        <div className="calories">
+          <img src={Calories} alt="cal" />
+          <div>
+            <span>Calories Burned</span>
+            <span>220 kcal</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
