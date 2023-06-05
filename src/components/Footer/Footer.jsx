1aec8cda0096aa2991855a053c4d7f30e56d9606
Rafaqat Ali <83506643+mianrafaqat@users.noreply.github.com>
2023-06-05 13:46:15 +0500

    footer secion

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Footer.css";
+import Github from "../../assets/github.png";
+import intsa from "../../assets/instagram.png";
+import linkedIn from "../../assets/linkedin.png";
+import logo from "../../assets/logo.png";
+
+const Footer = () => {
+  return (
+    <div className="Footer-container">
+      <hr />
+      <div className="footer">
+        <div className="social-links">
+          <img src={Github} alt="footer" />
+          <img src={intsa} alt="footer" />
+          <img src={linkedIn} alt="footer" />
+        </div>
+
+        <div className="logo-f">
+          <img src={logo} alt="f-logo" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
